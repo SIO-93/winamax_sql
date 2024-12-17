d9ea8b14cd951ea2d94290154bda39e0dfd87663
malbar974 <thomas.rayane.sio@gmail.com>
2024-12-17 09:50:22 +0100

    Update insert.sql
    insert into

---
+++ insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO exams (id, matiere, date, sujet)
+VALUES (1, 'Mathématiques', '2024-12-17', 'jd'),
+       (2, 'tc2', '2024-12-17', 'reseau');
+
+INSERT INTO bets (id, exam, user_1, user_2, amount)
+VALUES (1, 1, 1, 2, 50), 
+       (2, 2, 2, 1, 75);
